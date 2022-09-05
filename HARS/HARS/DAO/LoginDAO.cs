8cf09c67dc2b94a94d8773fdc6a5dd6588e3842d
Ashar Habib <ashar_habib@hotmail.com>
2022-09-05 16:23:30 -0500

    env vars

--- HARS/HARS/DAO/LoginDAO.cs
+++ HARS/HARS/DAO/LoginDAO.cs
@@ -11,11 +11,16 @@ namespace HARS.DAO
     public class LoginDAO
     {
         private static string connectionString = "";
+        static LoginDAO()
+        {
+            connectionString = "Server="+Environment.GetEnvironmentVariable("DB_URL")+ ";Port=5432;Database="+Environment.GetEnvironmentVariable("DB_NAME")+ ";User Id="
+                +Environment.GetEnvironmentVariable("DB_USERNAME")+ ";Password=" + Environment.GetEnvironmentVariable("DB_PASSWORD");
+        }
         public static User VerifyLoginCredentials(string username, string password)
         {
             var conn = new NpgsqlConnection(connectionString);
             conn.Open();
-            string sql = "SELECT * FROM managers WHERE manager_username ='" + username + "' AND manager_password = '" + password + "';";
+            string sql = "SELECT * FROM users WHERE username ='" + username + "' AND user_password = '" + password + "';";
             var cmd = new NpgsqlCommand(sql, conn);
 
             NpgsqlDataReader rdr = cmd.ExecuteReader();
---
+++ HARS/HARS/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HARS.Models
+{
+    internal class Reservation
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public string reservationJSON { get; set; }
+        public string namesJSON { get; set; }
+    }
+}
